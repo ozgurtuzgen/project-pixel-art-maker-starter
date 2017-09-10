5d20c4050536e96f59d8d3fee4175c37513feb3f
Özgür TÜZGEN <otuzgen@stm.com.tr>
2017-09-10 09:44:47 +0300

    code review comments: validation and some fixes has been implemented

--- designs.js
+++ designs.js
@@ -1,24 +1,24 @@
 $(document).ready(function () {
     // Select color input
-    let color = $("#colorPicker").val();
+    let colorPicker = $("#colorPicker");
 
     // Select size input
-    let width = $("#input_width").val();
-    let height = $("#input_height").val();
+    let input_width = $("#input_width");
+    let input_height = $("#input_height");
 
     // When size is submitted by the user, call makeGrid()
 
     $("#sizePicker").submit(function(event){
-        width = $("#input_width").val();
-        height = $("#input_height").val();
+        width = input_width.val();
+        height = input_height.val();
         makeGrid();
 
         return false;
     });
 
-    $("#colorPicker").on("change",function () {
-        color = $("#colorPicker").val();
-    });
+    // $("#colorPicker").on("change",function () {
+    //     color = colorPicker.val();
+    // });
 
     function makeGrid() {
         let table = $("#pixel_canvas")[0];
@@ -26,18 +26,18 @@ $(document).ready(function () {
             table.removeChild(table.firstChild);
         }
 
-        for (let i = 0; i < height; i++) {
+        for (let i = 0; i < input_height.val(); i++) {
             let tr = document.createElement('tr');
-            for (let j = 0; j < width; j++) {
+            for (let j = 0; j < input_width.val(); j++) {
                 let td = document.createElement('td');
 
                 td.onclick = (function (r, c) {
                     return function () {
-                        if (td.bgColor == color) {
+                        if (td.bgColor == colorPicker.val()) {
                             td.bgColor = "#ffffff";
                         }
                         else {
-                            td.bgColor = color;
+                            td.bgColor = colorPicker.val();
                         }
                     };
                 }(i, j));
